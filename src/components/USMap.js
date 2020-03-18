fb66b97d6f95560c2ff3d5d2fff9306edcffc975
Dan Spector <danno2423@gmail.com>
2020-03-18 15:02:34 -0400

    generated map

--- src/components/USMap.js
+++ src/components/USMap.js
@@ -1,18 +1,26 @@
 import React, { useEffect, useState, useRef } from 'react';
 import * as d3 from 'd3';
+import { set } from 'd3';
 
 const USMap = () => {
 
-  const [svg, setSvg] = useState();
+  const [geojson, setGeoJson] = useState();
   const mapContainer = useRef(null);
+  const width = 900;
+  const height = 600;
 
+  //fetches geoJson
   useEffect(() => {
+
+    // //synchronous
+    // const jsonRes = JSON.parse('./data/us_states.json');
+    // setGeoJson(jsonRes.features);
+
+    // asynchronous
     const fetchSvg = async () => {
       try {
-        const svgRes = await d3.svg('./data/us_counties.svg')
-        console.log(svgRes.documentElement);
-        setSvg(svgRes.documentElement);
-
+        const jsonRes = await d3.json('./data/us_states.json');
+        setGeoJson(jsonRes.features);
       } catch (e) {
         console.error(e);
       }
@@ -21,15 +29,42 @@ const USMap = () => {
     fetchSvg();
   }, []);
 
-  if (svg && mapContainer.current) {
-    //d3 work here...
-  }
+  const projection = d3.geoAlbersUsa()
+    .translate([width / 2, height / 2])
+    .scale(width * 1.25);
 
-  return (
-    <div ref={mapContainer}>
-      USMap
-    </div>
-  );
+  const geoPath = d3.geoPath()
+    .projection(projection);
+
+  // if (svg && mapContainer.current) {
+  //   //d3 work here...
+  // }
+
+  return geojson
+  ?
+  (
+    <svg className="USMap"
+      width={width}
+      height={height}
+    >
+      <g className="geoMap_national">
+        {geojson.map(d => console.log(d))}
+        {geojson.map((d) => 
+          <path 
+            key={d.properties.NAME}
+            d={geoPath(d)}
+            className='state'
+            id={d.properties.NAME}
+            strokeWidth={0.25}
+            stroke='black'
+            fill='transparent'
+          />
+        )}
+      </g>
+    </svg>
+  )
+  :
+  null;
 }
 
 export default USMap;
