a759d04343a24d290ad25b217a2915481ba738cd
Dan Spector <danno2423@gmail.com>
2020-03-19 15:05:28 -0400

    working on slider

--- src/components/TimeSlider.js
+++ src/components/TimeSlider.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import { Slider } from '@material-ui/core';
+import { min, max } from 'd3-array';
+import moment from 'moment';
 
-const TimeSlider = () => {
+const TimeSlider = ({ nationalData }) => {
+
+  const days = nationalData.map(entry => {
+    const day = entry.date.dayOfYear();
+    return day;
+  });
+
+  const valueLabelFormat = (value) => {
+    //fixes bug where first value of slider is NaN until slider is clicked
+    if (isNaN(value)) {
+      value = min(days);
+    }
+    return moment(value, 'DDD DDDD').format('MMM D');
+  }
 
   return (
-    <Slider />
+    <Slider 
+      min={min(days)}
+      max={max(days)}
+      valueLabelDisplay="auto"
+      valueLabelFormat={valueLabelFormat}
+    />
   );
 }
 
