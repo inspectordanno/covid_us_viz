8b181f62c2f2b4e30f0bb38ce49d9520bce7278a
Dan Spector <danno2423@gmail.com>
2020-05-16 19:43:12 -0400

    Update Legend.js

--- src/components/Legend/Legend.js
+++ src/components/Legend/Legend.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { format } from 'd3-format';
+import { scaleThreshold } from '@vx/scale';
+import { LegendThreshold, LegendItem, LegendLabel } from '@vx/legend';
+
 
 import styles from '../Header/header.module.scss';
 
@@ -6,7 +10,26 @@ const Legend = () => {
 
   return (
     <div className={styles.legend}>
-      
+      <LegendThreshold scale={thresholdScale}>
+          {labels => {
+            return labels.reverse().map((label, i) => {
+              const size = 15;
+              return (
+                <LegendItem
+                  key={`legend-quantile-${i}`}
+                  margin="1px 0"
+                >
+                  <svg width={size} height={size}>
+                    <rect fill={label.value} width={size} height={size} />
+                  </svg>
+                  <LegendLabel align={'left'} margin={'2px 0 0 10px'}>
+                    {label.text}
+                  </LegendLabel>
+                </LegendItem>
+              );
+            });
+          }}
+        </LegendThreshold>
     </div>
   )
 }
