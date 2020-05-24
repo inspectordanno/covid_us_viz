03b68c293e4aa5b34aa9eae71ef36d3592b5b960
Dan Spector <danno2423@gmail.com>
2020-05-24 14:57:17 -0400

    fixing select boxes

--- src/components/CountySelect/CountySelect.js
+++ src/components/CountySelect/CountySelect.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Select from 'react-select';
 import { useDispatch } from 'react-redux';
 
 import stateFipsDict from '../../data/state_fips_dict.json';
 import { dispatchCountyFips } from '../../actions/actions';
-// import { randomCounty } from '../../reducers/reducer';
+import usePrevious from '../../hooks/usePrevious';
 
 const CountySelect = ({ countyData, UsState }) => {
 
   const dispatch = useDispatch();
+  const [selectValue, setSelectValue] = useState();
+  const prevUsState = usePrevious(UsState);
+
+  useEffect(() => {
+    if (prevUsState !== UsState) {
+      setSelectValue(null);
+    } 
+  },[UsState])
 
   const counties = stateFipsDict[UsState];
   const validCounties = counties.filter(county => countyData.has(county.fips));
-
   const options = validCounties.map(county => ({ value: county.fips, label: county.countyName }));
 
   const handleOnChange = (option) => {
+    setSelectValue(option);
     dispatch(dispatchCountyFips(option.value));
   }
 
@@ -24,6 +32,8 @@ const CountySelect = ({ countyData, UsState }) => {
       className={'select'}
       options={options}
       onChange={handleOnChange}
+      value={selectValue}
+      isClearable={true}
     />
   )
 }
--- src/components/UsStateSelect/UsStateSelect.js
+++ src/components/UsStateSelect/UsStateSelect.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Select from 'react-select';
 import { useDispatch } from 'react-redux';
 
@@ -6,13 +6,16 @@ import stateFipsDict from '../../data/state_fips_dict.json';
 import { dispatchUsState } from '../../actions/actions';
 import { reducerDefaultState } from '../../reducers/reducer';
 
-const UsStateSelect = () => {
+const UsStateSelect = ({ UsState }) => {
 
   const dispatch = useDispatch();
+
+  const [selectValue, setSelectValue] = useState({ value: UsState, label:UsState }); //set default state
 
   const UsStates = Object.keys(stateFipsDict).map(state => ({ value: state, label: state }));
 
   const handleOnChange = (option) => {
+    setSelectValue(option);
     dispatch(dispatchUsState(option.value));
   }
 
@@ -21,6 +24,7 @@ const UsStateSelect = () => {
       className={'select'}
       options={UsStates}
       onChange={handleOnChange}
+      value={selectValue}
     />
   )
 } 
