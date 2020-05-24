93ce986fcfac02c6dfe56b3845418ee9dce7a310
Dan Spector <danno2423@gmail.com>
2020-05-23 20:00:48 -0400

    Update createCountyStateDict.js

--- src/node/createCountyStateDict.js
+++ src/node/createCountyStateDict.js
@@ -9,7 +9,7 @@ csv()
     const stateFipsMap = {};
     grouped.forEach((value, key) => {
       const counties = value.map(county => { 
-        if (county.fips.length === 4) {
+        if (county.fips.length === 4 && +county.fips) {
           county.fips = '0' + county.fips; //add 0 to 4-digit fips codes
         }
         return { countyName: county.county_name, fips: county.fips }
