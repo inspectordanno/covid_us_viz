844d484e992b219123db093159d959a3a0127b7b
Dan Spector <danno2423@gmail.com>
2020-06-02 18:21:34 -0400

    Update reducer.js

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,17 +1,4 @@
-import stateFipsDict from '../data/state_fips_dict.json';
-
-// const randomProperty = (obj) => {
-//   const keys = Object.keys(obj);
-//   return keys[keys.length * Math.random() << 0];
-// };
-
-// const randomState = randomProperty(stateFipsDict);
-// const randomCounties = stateFipsDict[randomState];
-// export const randomCounty = randomCounties[[Math.floor(Math.random() * randomCounties.length)]];
-
 export const reducerDefaultState = {
-  // dateIndex: 0,
-  // dateIncrement: true, //true, false
   UsState: '',
   county: '',
   measure: 'newCases' //totalCases, totalDeaths, newCases, newDeaths, percentChangeCases, percentChangeDeaths
@@ -19,16 +6,6 @@ export const reducerDefaultState = {
 
 export default (state = reducerDefaultState, action) => {
   switch (action.type) {
-    case 'DATE_INDEX':
-      return {
-        ...state,
-        dateIndex: action.dateIndex
-      }
-    case 'DATE_INCREMENT':
-      return {
-        ...state,
-        dateIncrement: action.dateIncrement
-      }
     case 'US_STATE':
       return {
         ...state,
